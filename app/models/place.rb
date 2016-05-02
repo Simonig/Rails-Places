813a61eb594b329ceb7a36e079e5e7335d56f504
Simon <simon.ig13@gmail.com>
2016-05-02 00:41:06 -0300

    geo 3

--- app/models/place.rb
+++ app/models/place.rb
@@ -54,6 +54,63 @@ class Place
         .delete_one
   end
 
+  def self.get_address_components(sort={:_id=>1}, offset = 0, limit = nil)
+    pipe=[]
+    pipe << {:$project=>{:address_components=>1, :formatted_address=>1, "geometry.geolocation":1}}
+    pipe << {:$unwind=>'$address_components'}
+    pipe << {:$sort=>sort} if !sort.nil?
+    pipe << {:$skip=>offset} if !offset.nil?
+    pipe << {:$limit=>limit} if !limit.nil?
+
+    result = self.collection.aggregate(pipe)
+
+  end
+
+  def self.get_country_names
+
+    result = []
+    result << {:$project=>{ "address_components.long_name"=>1, "address_components.types"=>1}}
+    result << {:$unwind=>'$address_components'}
+    result << {:$match=>{"address_components.types" =>'country'}}
+    result << {:$group=>{ :_id => "$address_components.long_name"}}
+    results = collection.aggregate(result)
+
+    return    results.to_a.map {|h| h[:_id]}
+  end
+
+  def self.find_ids_by_country_code (country_code)
+
+    result = collection.aggregate([{:$match=>{"address_components.short_name" => country_code }}])
+    return result.map {|doc| doc[:_id].to_s}
+
+  end
+  def self.create_indexes
+
+    collection.indexes.create_one({"geometry.geolocation": Mongo::Index::GEO2DSPHERE})
+
+  end
+
+  def self.remove_indexes
+    collection.indexes.drop_one('geometry.geolocation':Mongo::Index::GEO2DSPHERE)
+
+  end
+
+  def self.near point, max_meters=nil
+
+    near_query={:$geometry=>point.to_hash}
+
+    near_query[:$maxDistance]=max_meters if max_meters
+
+    collection.find(:"geometry.geolocation"=>{:$near=>near_query})
+
+  end
+
+  def near max_meters=nil
+
+    self.class.to_places(self.class.near(location, max_meters))
+
+  end
+
 
     def initialize(hash = {})
       @address_components = []
