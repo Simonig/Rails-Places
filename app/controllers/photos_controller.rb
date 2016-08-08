f40adf22d7a1da78d8b2931cd818c136b122aa7f
Simon <simon.ig13@gmail.com>
2016-08-08 00:42:41 -0400

    serve photo finish

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,10 @@
+class PhotosController < ApplicationController
+
+  def show
+
+    @photo = Photo.find(params[:id])
+
+    send_data @photo.contents, { type: 'image/jpeg', disposition: 'inline'}
+
+  end
+end
